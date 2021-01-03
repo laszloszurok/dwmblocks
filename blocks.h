e01d367d4bcac53bd92eb052e8ff4c13e909216b
László Szurok <szuroklaszlo97@gmail.com>
2021-01-03 10:01:41 +0100

    removed unused blocks, added a new 'todos' block

--- blocks.h
+++ blocks.h
@@ -2,14 +2,12 @@
 // │
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		        /*Update Interval*/	   /*Update Signal*/
-    { "  ",      "~/source/scripts/status/weather",             0,    20 },
-    { "   ",     "~/source/scripts/status/wifinetwork",         0,    15 },
-    { "   ",     "~/source/scripts/status/newsboat_status",     7200, 21 },
-    { "   ",     "~/source/scripts/status/sys_resources",       0,    18 },
+    { "  ",      "~/source/scripts/status/wifinetwork",         0,    15 },
+    { "   ",     "~/source/scripts/status/newsboat_status",  7200,    21 },
     { "   ",     "~/source/scripts/status/disk",                0,    16 },
-    { "   ",     "~/source/scripts/status/kblayout",            0,    12 },
     { "   ",     "~/source/scripts/status/volume",              0,    10 },
     { "   ",     "~/source/scripts/status/battery",             5,    17 },
+    { "   ",     "~/source/scripts/status/todos",           14400,    14 },
     { "   ",     "~/source/scripts/status/clock",               5,    13 },
 };
 
