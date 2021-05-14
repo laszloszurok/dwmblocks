12ed9f995da648d83ae8f318628013cc1a11fc4f
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2021-05-14 17:52:15 +0200

    updated status blocks

--- blocks.h
+++ blocks.h
@@ -3,11 +3,10 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		        /*Update Interval*/	   /*Update Signal*/
     { "  ",      "~/source/scripts/status/wifinetwork",         0,    15 },
-    { "   ",     "~/source/scripts/status/vpn",                 0,    12 },
-    { "   ",     "~/source/scripts/status/mail",                0,    11 },
+    { "   ",     "~/source/scripts/status/mic",                 0,    12 },
     { "   ",     "~/source/scripts/status/volume",              0,    10 },
     { "   ",     "~/source/scripts/status/battery",             5,    17 },
-    { "   ",     "~/source/scripts/status/todos",           14400,    14 },
+    { "   ",     "~/source/scripts/status/kblayout",            0,    14 },
     { "   ",     "~/source/scripts/status/clock",               5,    13 },
 };
 
