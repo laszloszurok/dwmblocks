746a6ce83617b1495dd76353a67b42826e05442d
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2021-06-04 15:01:42 +0200

    added new status blocks

--- blocks.h
+++ blocks.h
@@ -5,9 +5,11 @@ static const Block blocks[] = {
     { "  ",      "~/source/scripts/status/wifinetwork",         0,    15 },
     { "   ",     "~/source/scripts/status/mic",                 0,    12 },
     { "   ",     "~/source/scripts/status/volume",              0,    10 },
-    { "   ",     "~/source/scripts/status/battery",             5,    17 },
+    { "   ",     "~/source/scripts/status/battery",            10,    17 },
+    { "   ",     "~/source/scripts/status/run_menu",            0,    11 },
     { "   ",     "~/source/scripts/status/kblayout",            0,    14 },
     { "   ",     "~/source/scripts/status/clock",               5,    13 },
+    { "  ",      "~/source/scripts/status/power_menu",          0,    16 },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
