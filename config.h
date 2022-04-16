bd9af5c9db9ed2c8d8c0ea62e2d51f71d6f1338f
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2022-04-16 12:37:43 +0200

    updated wifi script more frequently

--- config.h
+++ config.h
@@ -1,14 +1,13 @@
 #define CMDLENGTH 60
 #define DELIMITER "    "
 #define CLICKABLE_BLOCKS
-#define LEADING_DELIMITER
 
 const Block blocks[] = {
     /* cmd, interval, signal */
-	BLOCK("~/source/scripts/status/wifinetwork",    30, 1),
-	BLOCK("~/source/scripts/status/mic",             0, 2),
-	BLOCK("~/source/scripts/status/volume",          0, 3),
-	BLOCK("~/source/scripts/status/battery",        45, 4),
-	BLOCK("~/source/scripts/status/kblayout",        0, 5),
-	BLOCK("~/source/scripts/status/clock",           5, 6),
+	BLOCK("~/source/scripts/status/wifinetwork",  5, 1),
+	BLOCK("~/source/scripts/status/mic",          0, 2),
+	BLOCK("~/source/scripts/status/volume",       0, 3),
+	BLOCK("~/source/scripts/status/battery",     45, 4),
+	BLOCK("~/source/scripts/status/kblayout",     0, 5),
+	BLOCK("~/source/scripts/status/clock",        5, 6),
 };
